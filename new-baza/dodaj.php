d65a7a3f14574a64fe51d1cc09d706a22d99fa66
Oliwier787 <127290369+Oliwier787@users.noreply.github.com>
2024-10-02 10:39:31 +0200

    Update dodaj.php

--- new-baza/dodaj.php
+++ new-baza/dodaj.php
@@ -4,4 +4,18 @@ $pol = mysqli_connect("localhost" , "root", "", "new-baza" );
 $imie = $_POST['Imie'];
 $nazwisko = $_POST['Nazwisko'];
 $dodaj = mysqli_query($pol,"INSERT INTO ludzie (Imie, Nazwisko) VALUES('$imie','$nazwisko')");
+
+echo "<br>Dane z tabeli <br>";
+$wynik = mysqli_query($pol,"SELECT `imie`,`nazwisko`FROM `ludzie` WHERE 1");
+if (mysqli_num_rows($wynik)>0);
+{
+    echo "<table bgcolor=gray border=1>";
+    echo "<td> Imie <td>";
+    echo "Nazwisko"; }
+    while ($dw = mysqli_fetch_array($wynik))
+    {
+        echo "<tr>";
+        echo "<td bgcolor=white-gray>".$dw['imie']."</td>";
+        echo "<td bgcolor=white-smoke>".$dw['nazwisko']."</td>";
+    }
 ?>
